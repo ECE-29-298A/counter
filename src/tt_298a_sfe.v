d99931e7eff77a018c6cdd4c553e29d3be34001e
Saahmuael <zhang.j.sam@gmail.com>
2026-09-20 13:37:56 -0400

    Update tt_298a_sfe.v

--- src/tt_298a_sfe.v
+++ src/tt_298a_sfe.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_298a_sfe (
+module tt_um_298a_sfe (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
