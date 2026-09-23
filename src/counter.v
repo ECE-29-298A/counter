63e6af0a890c21b763529241c5218153e97521f0
Felix Deng <dengjianhao22@gmail.com>
2026-09-22 23:13:30 -0400

    counter - Felix

--- src/counter.v
+++ src/counter.v
@@ -1,36 +1,17 @@
 module counter (
-    input wire clk,
-    input wire rst_n,
-    output wire [7:0] countval
+    input clk,
+    input reset,
+    input tristate,
+    output [7:0] count
 );
 
-reg [7:0] counter;
-reg [1:0] rsync;
-
-always@(posedge clk or negedge rst_n) begin 
-    if (!rst_n) begin 
-        rsync <= 2'b00;
-    end else begin 
-        rsync <= {rsync[0], 1'b1};
+    reg [7:0] counter;
+    always@(posedge clk or posedge reset) begin
+        if(reset)
+            counter <= 8'b0;
+        else
+            counter <= counter +1;
     end
-end
 
-always@(posedge clk or negedge rst_n) begin 
-    if (!rst_n) begin 
-        counter <= 8'h00;
-    end 
-    else if (!rsync[1]) begin 
-        counter <= 8'h00;
-    end 
-    else begin 
-        if (counter != 8'hFF) begin 
-            counter <= counter + 1;
-        end else begin 
-            counter <= 8'h00;
-        end
-    end
-end
-
-assign countval = counter;
-    
+    assign count = tristate ? 8'bz : counter;
 endmodule
--- src/tristate.v
+++
@@ -1,9 +0,0 @@
-module tristate (
-    input wire in,
-    input wire tricont,
-    output wire out
-);
-
-assign out = (tricont) ? in : 1'bz;
-    
-endmodule
